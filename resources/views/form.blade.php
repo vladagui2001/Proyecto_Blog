40a1df55110c74237db236441f7d8101955c277b
vladagui2001 <vlad.aguiluz5c@gmail.com>
2022-10-31 13:37:12 -0600

    modified:   composer.json 	modified:   composer.lock 	modified:   config/app.php 	modified:   resources/views/form.blade.php

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,19 +1,18 @@
 @extends('layouts.app')
 @section('title', 'Trainers Create')
 @section('content')
-<form class="form-group" method="POST" action="/trainers" enctype="multipart/form-data"> 
-    @csrf
-         <div clas='form-group'>
-             <label for=''>Nombre</label>
-             <input type='text'name="name"class='form-control'>
-             <label for=''>Apellido</label>
-             <input type='text'name="Apellido"class='form-control'>
-         </div>
-         <div clas='form-group'>
-            <label for="">Avatar:</label>
-            <input type="file" name="avatar">
-        </div>
-         <button type='submit'class='btn btn-primary'>
-        Guardar</button>
-</form>
+
+{!! Form::open(['route'=>'trainers.store', 'method'=>'POST','files'=>'true']), !!}
+<div clas="form-group">
+    {{Form::label('name','Nombre')}}
+    {{Form::text('name',null,['class'=>'form-control'])}}
+    {{Form::label('apellido','Apellido')}}
+    {{Form::text('apellido',null,['class'=>'form-control'})}}
+</div>
+<div clas="form-group">
+    {{Form::label('avatar','Avatar')}}
+    {{Form::file('avatar')}}
+</div>
+{{Form::submit('Guardar',['class'=>'btn btn-primary'])}}
+{!!Form::close()!!}
 @endsection
